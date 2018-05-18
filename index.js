17a9d93587677aff9a68b13154eabbb2ff16e42f
Trevor Russell <truss.42@gmail.com>
2018-05-18 16:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber (katzDeliLine, name) {
   katzDeliLine.push(name);
   var number = katzDeliLine.length
-  return(`"Welcome, ${name}. You are number ${number} in line."`)
+  return(`Welcome, ${name}. You are number ${number} in line.`)
 }
